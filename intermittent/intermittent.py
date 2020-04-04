a6e2fd41992de9c1a430a5c0738fc4ec1c4287c8
jingw2 <jingw2@foxmail.com>
2020-04-04 17:05:36 +0800

    Update intermittent.py

--- intermittent/intermittent.py
+++ intermittent/intermittent.py
@@ -16,6 +16,7 @@ import adjust_croston
 import kalman
 import matplotlib.pyplot as plt
 import slow_move
+import multistage_glm
 import scipy.stats as st 
 import math 
 
@@ -52,9 +53,6 @@ class IntermitentForcaster:
         kf.fit(y)
         for mu in kf.mus:
             yfit.append(H.dot(mu)[0])
-        
-        for mu, var in zip(kf.mus, kf.Vs):
-            st.norm.cdf(0.25, loc=mu, scale=math.sqrt(var))
         for _ in range(forecast_period):
             ypred.append(H.dot(kf.predict()))
         yfit = np.asarray(yfit).reshape((-1, 1))
@@ -91,18 +89,53 @@ class IntermitentForcaster:
             ypred25.append(p25)
             ypred90.append(p90)
         return y50, ypred
+    
+    @staticmethod
+    def multistage(X, y, forecast_period, num_sample_points=200):
+        glm = multistage_glm.MultiStageGLM()
+        Xtrain = X[-forecast_period:]
+        ytrain = y[-forecast_period:]
+        Xtest = X[forecast_period:]
+        ytest = y[forecast_period:]
+        glm.fit(Xtrain, ytrain)
+        yfit = glm.predict(Xtrain)
+        ypred_list = []
+        for _ in range(num_sample_points):
+            ypred = glm.predict(Xtest)
+            ypred_list.append(ypred.reshape((-1, 1)))
+        ypred = np.concatenate(ypred_list, axis=1)
+        
+        p50 = np.quantile(ypred, 0.5, axis=1)
+        p25 = np.quantile(ypred, 0.25, axis=1)
+        p90 = np.quantile(ypred, 0.90, axis=1)
+
+        return yfit, p50
+
 
 if __name__ == "__main__":
     # a = np.zeros(100) 
     # val = np.array(random.sample(range(10,200), 20)) 
-    num_points = 100
-    idxs = random.sample(range(num_points), 60)
-    # ts = np.insert(a, idxs, val)
-    tot_ts = np.random.randint(size=(100,), low=1, high=20)
-    tot_ts[idxs] = 0
-    train_size = 80
+    # num_points = 100
+    # idxs = random.sample(range(num_points), 60)
+    # # ts = np.insert(a, idxs, val)
+    # tot_ts = np.random.randint(size=(100,), low=1, high=20)
+    # tot_ts[idxs] = 0
+    # train_size = 80
+    # ts = tot_ts[:train_size]
+    # forecast_period = num_points - train_size
+    import pandas as pd 
+    data = pd.read_csv("intermittent_data.csv", parse_dates=["date"])
+    data["day_of_week"] = data["date"].apply(lambda x: x.dayofweek)
+    data["month"] = data["date"].apply(lambda x: x.month)
+    data["year"] = data["date"].apply(lambda x: x.year)
+    num_data = len(data)
+    train_ratio = 0.8
+    train_size = int(num_data * train_ratio)
+    test_size = num_data - train_size
+    tot_ts = data["sales"]
+    X = np.c_[np.asarray(pd.get_dummies(data["day_of_week"])), np.asarray(pd.get_dummies(data["month"]))]
     ts = tot_ts[:train_size]
-    forecast_period = num_points - train_size
+    forecast_period = test_size
     yfit, ypred = IntermitentForcaster.adjust_croston(ts, forecast_period)
     yhat1 = np.concatenate([yfit, ypred])
 
@@ -121,6 +154,9 @@ if __name__ == "__main__":
     yfit, ypred = IntermitentForcaster.slow_mover(ts, forecast_period)
     yhat6 = np.concatenate([yfit, ypred])
 
+    yfit, ypred = IntermitentForcaster.multistage(X, ts, forecast_period)
+    yhat7 = np.concatenate([yfit, ypred])
+
     plt.plot(tot_ts) 
     plt.plot(yhat1)
     plt.plot(yhat2)
@@ -128,7 +164,8 @@ if __name__ == "__main__":
     plt.plot(yhat4)
     plt.plot(yhat5)
     plt.plot(yhat6)
-    plt.legend(["True", "adjusted_croston_hyper", "adjust_croston", "croston", "filter", "smoother", "slow_move"])
+    plt.plot(yhat7)
+    plt.legend(["True", "adjusted_croston_hyper", "adjust_croston", "croston", "filter", "smoother", "slow_move", "multistage"])
     ymin, ymax = plt.ylim()
     plt.vlines(train_size, ymin, ymax, color="k", linestyles="dashed", linewidth=2)
     plt.ylim(ymin, ymax)
